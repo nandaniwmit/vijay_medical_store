23d7cc494e1af184174976c96183f0e90d4a4d2e
nandaniwmit <nandani.wmit@gmail.com>
2026-08-12 20:40:19 +0530

    Update tracking endpoint URL in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import { STORE_INFO } from '../data/pharmacyData';
 export const Footer: React.FC = () => {
   // Global tracking hook integrated exactly as specified in the prompt
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
